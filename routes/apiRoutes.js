480d0eea9c47356c7c2b6062b8690ad547ffe534
Pablo Ontiveros <pablacas@gmail.com>
2020-08-28 04:16:36 -0500

    finished, i think

---
+++ routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        Workout.create(req.body)
+            .then(dbWorkouts => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.findOneAndUpdate(
+            { id: req.params.id },
+            { $push: { exercises: req.body } },
+            {new: true, runValidators: true}
+        )
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+}
